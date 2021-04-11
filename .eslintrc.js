899b2bd10dd8d4f27e8813f31bb8aecd781ed098
Quido Hoekman <quido@qhoekman.nl>
2021-04-11 13:59:51 +0200

    chore(eslint): Added import plugin for sorting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,14 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
-
-    // Prettier plugin and recommended rules
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   rules: {
-    // Include .prettierrc.js rules
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-
     'react/prop-types': 'off',
     'padding-line-between-statements': [
       'error',
